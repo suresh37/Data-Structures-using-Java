640edd149207626d6e87f8931439f8ba5a7468b8
suresh37 <sureshkarthick37@gmail.com>
2017-12-25 21:03:54 +0530

    Update LinkedList.java

--- DataStructuresJava/src/LinkedList.java
+++ DataStructuresJava/src/LinkedList.java
@@ -26,7 +26,7 @@ public class LinkedList
 	}
 	public void insertAtStart(int data)
 	{
-		Node node = new Node();
+		Node node = new Node(); //
 		node.data = data;
 		node.next = null;
 		node.next = head;
